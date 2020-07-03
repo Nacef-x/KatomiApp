d23e93871df9719f6c8d40df1a88bf4c582e7cf6
Nacef-x <khadhraouinacef@gmail.com>
2020-07-03 15:27:28 +0100

    event fetch 3nd commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
+import { AppRegistry, Platform } from 'react-native';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('KatomiApp', () => App);
+
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('main');
+  AppRegistry.runApplication('KatomiApp', { rootTag });
+}
